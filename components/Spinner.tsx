4336dff09ca9c3a97669081ac6d2c8333e9b37d8
Craig Samad <craig.samad@ascendum.com>
2021-01-18 11:16:50 -0500

    added emotion packages, test components

---
+++ components/Spinner.tsx
@@ -0,0 +1,33 @@
+/** @jsxImportSource @emotion/react */
+import { keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import { FaSpinner } from 'react-icons/fa'
+
+const spin = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+})
+
+export const Spinner = styled(FaSpinner)({
+  animation: `${spin} 1s linear infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading',
+}
+
+export function FullPageSpinner() {
+  return (
+    <div
+      css={{
+        fontSize: '4em',
+        height: '30vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Spinner />
+    </div>
+  )
+}
