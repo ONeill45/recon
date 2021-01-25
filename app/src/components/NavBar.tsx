eb1318208a1f4a4063407d49a6faa421f38f54ff
Kealoha Ching <kealoha.ching@ascendum.com>
2021-01-25 10:39:00 -0700

    Added `displayed` property to NavBar inner components

--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,26 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 import Image from 'next/image'
 import { NavButtons } from './'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { DisplayType } from '../interfaces'
 
+type displayProps = {
+  displayed: boolean
+}
+
 const MainDiv = styled.div`
   height: 60px;
   background-color: orange;
 `
-const FullNavDiv = styled.div`
-  display: flex;
+
+const isDisplayed = ({ displayed }: displayProps) => {
+  if (displayed)
+    return css`
+      display: flex;
+    `
+  return css`
+    display: none;
+  `
+}
+
+const FullNavDiv = styled.div<displayProps>`
+  ${isDisplayed};
 `
-const CollapsedNavDiv = styled.div`
+const CollapsedNavDiv = styled.div<displayProps>`
+  ${isDisplayed};
   height: 100%;
   padding-left: 5px;
-  display: flex;
   justify-content: flex-start;
   align-items: center;
 `
 
-const SideNavDiv = styled.div`
+const SideNavDiv = styled.div<displayProps>`
+  ${isDisplayed};
   width: 25%;
   position: fixed;
   top: 60px;
@@ -72,30 +89,23 @@ export const NavBar = () => {
   return (
     <>
       <MainDiv>
-        {collapsed ? (
-          <CollapsedNavDiv>
-            <GiHamburgerMenu size="40px" onClick={handleHamburgerClick} />
-          </CollapsedNavDiv>
-        ) : (
-          <FullNavDiv>
-            <Image src="/images/recon-192x192.png" height="60" width="60" />
-            <NavButtons
-              buttonProperties={buttonProperties}
-              displayType={DisplayType.ROW}
-            />
-          </FullNavDiv>
-        )}
-      </MainDiv>
-      {displaySideMenu ? (
-        <SideNavDiv>
+        <CollapsedNavDiv displayed={collapsed}>
+          <GiHamburgerMenu size="40px" onClick={handleHamburgerClick} />
+        </CollapsedNavDiv>
+        <FullNavDiv displayed={!collapsed}>
+          <Image src="/images/recon-192x192.png" height="60" width="60" />
           <NavButtons
             buttonProperties={buttonProperties}
-            displayType={DisplayType.COLUMN}
+            displayType={DisplayType.ROW}
           />
-        </SideNavDiv>
-      ) : (
-        <></>
-      )}
+        </FullNavDiv>
+      </MainDiv>
+      <SideNavDiv displayed={displaySideMenu}>
+        <NavButtons
+          buttonProperties={buttonProperties}
+          displayType={DisplayType.COLUMN}
+        />
+      </SideNavDiv>
     </>
   )
 }
