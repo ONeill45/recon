50d6bd2017f441fe0eef3c9fd88ede5125974c41
Kealoha Ching <kealoha.ching@ascendum.com>
2021-01-27 12:43:44 -0700

    Linted file that did not get fixed in lint-fix

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,16 @@
-import "reflect-metadata";
-import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
+import 'reflect-metadata'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
 
-import { connect } from "./database";
-import { ResourceResolver } from "./resolvers";
+import { connect } from './database'
+import { ResourceResolver } from './resolvers'
 
 async function main() {
-  await connect();
-  const schema = await buildSchema({ resolvers: [ResourceResolver] });
-  const server = new ApolloServer({ schema });
-  await server.listen(process.env.PORT);
-  console.log("Server has started!");
+  await connect()
+  const schema = await buildSchema({ resolvers: [ResourceResolver] })
+  const server = new ApolloServer({ schema })
+  await server.listen(process.env.PORT)
+  console.log('Server has started!')
 }
 
-main();
+main()
