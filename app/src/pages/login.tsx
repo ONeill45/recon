1f5315961d4d18845d60bb2860b28902c454b976
David Samad <david.samad@ascendum.com>
2021-01-28 11:58:12 -0500

    Changed auth to use @azure/msal-browser

--- app/src/pages/login.tsx
+++ app/src/pages/login.tsx
@@ -1,59 +1,62 @@
 import React, { useEffect } from 'react'
 import { useAuthContext } from 'src/utils/context'
 import { useRouter } from 'next/router'
-import {
-  AzureAD,
-  AuthenticationState,
-  IAzureADFunctionProps,
-  LoginType,
-  MsalAuthProvider,
-} from 'react-aad-msal'
-import { LogLevel, Logger } from 'msal'
 
-const logger = new Logger(
-  (logLevel, message, containsPii) => {
-    console.log('[MSAL]', message)
-  },
-  {
-    level: LogLevel.Verbose,
-    piiLoggingEnabled: false,
-  },
-)
+import AzureAuthenticationButton from '../components/AzureAuthenticationButton'
+// import {
+//   AzureAD,
+//   AuthenticationState,
+//   IAzureADFunctionProps,
+//   LoginType,
+//   MsalAuthProvider,
+// } from 'react-aad-msal'
+// import { LogLevel, Logger } from 'msal'
 
-const authProvider = new MsalAuthProvider(
-  {
-    auth: {
-      authority: 'https://login.microsoftonline.com/common',
-      clientId: '0f2c6253-3928-4fea-b131-bf6ef8f69e9c', // need real value
-      postLogoutRedirectUri: window.location.origin,
-      redirectUri: window.location.origin,
-      validateAuthority: true,
+// const logger = new Logger(
+//   (logLevel, message, containsPii) => {
+//     console.log('[MSAL]', message)
+//   },
+//   {
+//     level: LogLevel.Verbose,
+//     piiLoggingEnabled: false,
+//   },
+// )
 
-      // After being redirected to the "redirectUri" page, should user
-      // be redirected back to the Url where their login originated from?
-      navigateToLoginRequestUrl: false,
-    },
-    // Enable logging of MSAL events for easier troubleshooting.
-    // This should be disabled in production builds.
-    system: {
-      logger: logger as any,
-    },
-    cache: {
-      cacheLocation: 'sessionStorage',
-      storeAuthStateInCookie: false,
-    },
-  },
-  {
-    scopes: ['openid'],
-  },
-  {
-    loginType: LoginType.Popup,
-    // When a token is refreshed it will be done by loading a page in an iframe.
-    // Rather than reloading the same page, we can point to an empty html file which will prevent
-    // site resources from being loaded twice.
-    tokenRefreshUri: window.location.origin + '/auth.html',
-  },
-)
+// const authProvider = new MsalAuthProvider(
+//   {
+//     auth: {
+//       authority:
+//         'https://login.microsoftonline.com/7f7697bc-3ee2-48f2-9d35-7cb75bddd74b',
+//       clientId: '8570b3ce-8294-4dba-9048-3f1f9f2eb7ad',
+//       postLogoutRedirectUri: 'http://localhost:3000/login', //window.location.origin,
+//       redirectUri: 'http://localhost:3000', // window.location.origin,
+//       validateAuthority: true,
+
+//       // After being redirected to the "redirectUri" page, should user
+//       // be redirected back to the Url where their login originated from?
+//       navigateToLoginRequestUrl: false,
+//     },
+//     // Enable logging of MSAL events for easier troubleshooting.
+//     // This should be disabled in production builds.
+//     system: {
+//       logger: logger as any,
+//     },
+//     cache: {
+//       cacheLocation: 'sessionStorage',
+//       storeAuthStateInCookie: false,
+//     },
+//   },
+//   {
+//     scopes: ['openid'],
+//   },
+//   {
+//     loginType: LoginType.Popup,
+//     // When a token is refreshed it will be done by loading a page in an iframe.
+//     // Rather than reloading the same page, we can point to an empty html file which will prevent
+//     // site resources from being loaded twice.
+//     tokenRefreshUri: 'http://localhost:3000/auth.html', // window.location.origin + '/auth.html',
+//   },
+// )
 
 const Login = () => {
   // @ts-ignore
@@ -74,42 +77,43 @@ const Login = () => {
   // }
 
   return (
-    <AzureAD provider={authProvider}>
-      {
-        // @ts-ignore
-        ({ login, logout, authenticationState }: IAzureADFunctionProps) => {
-          const isInProgress =
-            authenticationState === AuthenticationState.InProgress
-          const isAuthenticated =
-            authenticationState === AuthenticationState.Authenticated
-          const isUnauthenticated =
-            authenticationState === AuthenticationState.Unauthenticated
+    // <AzureAD provider={authProvider}>
+    //   {
+    //     // @ts-ignore
+    //     ({ login, logout, authenticationState }: IAzureADFunctionProps) => {
+    //       const isInProgress =
+    //         authenticationState === AuthenticationState.InProgress
+    //       const isAuthenticated =
+    //         authenticationState === AuthenticationState.Authenticated
+    //       const isUnauthenticated =
+    //         authenticationState === AuthenticationState.Unauthenticated
 
-          if (isAuthenticated) {
-            return (
-              <>
-                <p>You're logged in!</p>
-                <button onClick={logout} className="Button">
-                  Logout
-                </button>
-                {/* <GetAccessTokenButton provider={authProvider} />
-                <GetIdTokenButton provider={authProvider} /> */}
-              </>
-            )
-          } else if (isUnauthenticated || isInProgress) {
-            return (
-              <button
-                className="Button"
-                onClick={login}
-                disabled={isInProgress}
-              >
-                Login
-              </button>
-            )
-          }
-        }
-      }
-    </AzureAD>
+    //       if (isAuthenticated) {
+    //         return (
+    //           <>
+    //             <p>You're logged in!</p>
+    //             <button onClick={logout} className="Button">
+    //               Logout
+    //             </button>
+    //             {/* <GetAccessTokenButton provider={authProvider} />
+    //             <GetIdTokenButton provider={authProvider} /> */}
+    //           </>
+    //         )
+    //       } else if (isUnauthenticated || isInProgress) {
+    //         return (
+    //           <button
+    //             className="Button"
+    //             onClick={login}
+    //             disabled={isInProgress}
+    //           >
+    //             Login
+    //           </button>
+    //         )
+    //       }
+    //     }
+    //   }
+    // </AzureAD>
+    <AzureAuthenticationButton />
   )
 }
 
