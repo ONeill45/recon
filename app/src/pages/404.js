36eb6920259526340b3f51cf4fa11a9ec72dfdcb
Pankti Patel <pankti.patel@ascendum.com>
2021-02-02 14:31:22 -0500

    Added 404 Error page

---
+++ app/src/pages/404.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { Button } from '../components'
+import { useRouter } from 'next/router'
+
+export default function Custom404() {
+
+  const router = useRouter()
+  
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Recon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.description}>404 - Page Not Found</h1>
+        <Button 
+          color = "orange"
+          onClick={() => router.push('/')}
+        >
+          Go Home
+        </Button>
+      </main>
+    </div>
+  )
+}
