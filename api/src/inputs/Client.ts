2394857276bcd265a00f728f07cb75c502af2ed2
Craig Samad <craig.samad@ascendum.com>
2021-02-02 10:08:16 -0500

    added create client mutation, client input

---
+++ api/src/inputs/Client.ts
@@ -0,0 +1,22 @@
+import { InputType, Field } from 'type-graphql'
+
+@InputType()
+export class CreateClientInput {
+  @Field()
+  clientName: string
+
+  @Field()
+  description: string
+
+  @Field({ nullable: true })
+  logoUrl?: string
+
+  @Field()
+  startDate: Date
+
+  @Field()
+  createdBy: string
+
+  @Field()
+  updatedBy: string
+}
