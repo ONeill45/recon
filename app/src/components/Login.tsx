4bbeeffef2f228f30d4c74b1653e0fb28e920138
Scott Dougherty <scottdougherty@Ascendums-MacBook-Pro.local>
2021-02-03 17:20:11 -0500

    completed all tests

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -22,7 +22,7 @@ export const Login = () => {
     try {
       await instance.loginPopup(loginRequest)
     } catch (error) {
-      // need to implement a logger or better error catching logic
+      // TODO: need to implement a logger or better error catching logic
       console.error(error)
     }
   }
--- app/test/functions/msal.test.ts
+++ app/test/functions/msal.test.ts
@@ -1,29 +1,64 @@
-import { callMsGraph } from 'utils/functions/msal'
+import faker from 'faker'
+import { callMsGraph } from 'utils/functions'
 
-const mockBlob = { length: 0 }
-const mockFetch = (url: string) => {
-  switch (url) {
-    case '/blob': {
-      //   const mockSuccessResponse = {};
-      // const mockJsonPromise = Promise.resolve(mockSuccessResponse); // 2
-      // const mockFetchPromise = Promise.resolve({ // 3
-      //   json: () => mockJsonPromise,
-      // });
-      const response = { mockBlob }
-      return Promise.resolve({
-        blob: () => response,
-      })
-    }
-    default: {
-      const mockJsonResponse = new Response('')
-      return Promise.resolve(mockJsonResponse)
-    }
-  }
-}
-describe('msal', () => {
-  beforeAll(() => (global.fetch = jest.fn(() => mockFetch)))
-  it('returns a URL object from a blob', async () => {
-    const response = await callMsGraph('/blob', 'token', 'blob')
-    expect(response).toEqual(URL.createObjectURL(mockBlob))
+const blob = new Blob()
+const mockBlob = jest.fn().mockResolvedValue(blob)
+const mockJson = jest.fn().mockResolvedValue({})
+const mockedFetch = jest.fn(() => {
+  return Promise.resolve({
+    blob: mockBlob,
+    json: mockJson,
+  })
+})
+
+global.fetch = jest.fn().mockImplementation(mockedFetch)
+const urlResult = 'http://localhost:3000/test'
+global.URL.createObjectURL = jest.fn(() => urlResult)
+
+describe('callMsGraph', () => {
+  it('handles a blob response from fetch', async () => {
+    const endpoint = '/blob',
+      authToken = faker.random.alphaNumeric(20)
+    const response = await callMsGraph(endpoint, authToken, 'blob')
+
+    expect(mockedFetch).toHaveBeenCalledWith(endpoint, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'image/jpeg',
+      },
+    })
+    expect(mockBlob).toHaveBeenCalled()
+    expect(URL.createObjectURL).toHaveBeenCalledWith(blob)
+    expect(response).toEqual(urlResult)
+  })
+  it('handles a json response from fetch when a json responseType is specified', async () => {
+    const endpoint = '/json',
+      authToken = faker.random.alphaNumeric(20)
+    await callMsGraph(endpoint, authToken, 'json')
+
+    expect(mockedFetch).toHaveBeenCalledWith(endpoint, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    expect(mockJson).toHaveBeenCalled()
+  })
+
+  it('handles a json response from fetch when no responseType is specified', async () => {
+    const endpoint = '/json',
+      authToken = faker.random.alphaNumeric(20)
+    await callMsGraph(endpoint, authToken)
+
+    expect(mockedFetch).toHaveBeenCalledWith(endpoint, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+    })
+    expect(mockJson).toHaveBeenCalled()
   })
 })
