da837439e5cf8f51542288183ca1f99c68b828d2
Scott Dougherty <scottdougherty@Ascendums-MacBook-Pro.local>
2021-03-16 11:46:02 -0400

    added ThemeProvider to app

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
 import { ApolloProvider } from '@apollo/client'
-
-import client from '../lib/apolloClient'
 import { MsalProvider } from '@azure/msal-react'
 import { PublicClientApplication, Configuration } from '@azure/msal-browser'
 import {
@@ -11,6 +10,7 @@ import {
   UnauthenticatedTemplate,
 } from '@azure/msal-react'
 
+import client from '../lib/apolloClient'
 import { Login, NavBar } from 'components'
 import { loadEnvironmentVariable } from 'utils/functions'
 
@@ -31,6 +31,9 @@ const getMsalConfig = (): Configuration => ({
     cacheLocation: 'localStorage',
   },
 })
+const theme = {
+  fontFamily: 'Avenir Book, Arial, Verdana, sans-serif',
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   const msalInstance = new PublicClientApplication(getMsalConfig())
@@ -44,9 +47,11 @@ const App = ({ Component, pageProps }: AppProps) => {
       <MsalProvider instance={msalInstance}>
         <NavBar />
         <AuthenticatedTemplate>
-          <ApolloProvider client={client}>
-            <Component {...pageProps} />
-          </ApolloProvider>
+          <ThemeProvider theme={theme}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </ThemeProvider>
         </AuthenticatedTemplate>
         <UnauthenticatedTemplate>
           <Login />
