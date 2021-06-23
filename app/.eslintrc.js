084c42f7a39f9afe5a9c89fe38fe903f8e654610
Josh Marston <josh.marston@ascendum.com>
2021-06-23 15:12:33 -0400

    Removing eslint indent to prevent conflicts with prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
   rules: {
     //indent: ['error', 2, { SwitchCase: 1 }],
     indent: 'off',
-    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
